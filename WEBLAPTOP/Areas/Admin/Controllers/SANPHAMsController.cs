6f12326b46a6f51ee59575cb15d8f0947ce1e596
tung757 <tung24062004@gmail.com>
2025-09-30 02:18:33 +0700

    updatemoi

---
+++ WEBLAPTOP/Areas/Admin/Controllers/SANPHAMsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WEBLAPTOP.Models;
+
+namespace WEBLAPTOP.Areas.Admin.Controllers
+{
+    public class SANPHAMsController : Controller
+    {
+        private DARKTHESTORE db = new DARKTHESTORE();
+
+        // GET: Admin/SANPHAMs
+        public ActionResult Index()
+        {
+            var sANPHAMs = db.SANPHAMs.Include(s => s.DANHMUC);
+            return View(sANPHAMs.ToList());
+        }
+
+        // GET: Admin/SANPHAMs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SANPHAM sANPHAM = db.SANPHAMs.Find(id);
+            if (sANPHAM == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sANPHAM);
+        }
+
+        // GET: Admin/SANPHAMs/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID_DM = new SelectList(db.DANHMUCs, "ID_DM", "TenDM");
+            return View();
+        }
+
+        // POST: Admin/SANPHAMs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_SP,MaSP,TenSP,Gia,GiaBan,Mota,Status_SP,NgayTao,SoLuong,SoLuongBan,Images_url,ID_DM")] SANPHAM sANPHAM)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SANPHAMs.Add(sANPHAM);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID_DM = new SelectList(db.DANHMUCs, "ID_DM", "TenDM", sANPHAM.ID_DM);
+            return View(sANPHAM);
+        }
+
+        // GET: Admin/SANPHAMs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SANPHAM sANPHAM = db.SANPHAMs.Find(id);
+            if (sANPHAM == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID_DM = new SelectList(db.DANHMUCs, "ID_DM", "TenDM", sANPHAM.ID_DM);
+            return View(sANPHAM);
+        }
+
+        // POST: Admin/SANPHAMs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_SP,MaSP,TenSP,Gia,GiaBan,Mota,Status_SP,NgayTao,SoLuong,SoLuongBan,Images_url,ID_DM")] SANPHAM sANPHAM)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sANPHAM).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID_DM = new SelectList(db.DANHMUCs, "ID_DM", "TenDM", sANPHAM.ID_DM);
+            return View(sANPHAM);
+        }
+
+        // GET: Admin/SANPHAMs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SANPHAM sANPHAM = db.SANPHAMs.Find(id);
+            if (sANPHAM == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sANPHAM);
+        }
+
+        // POST: Admin/SANPHAMs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SANPHAM sANPHAM = db.SANPHAMs.Find(id);
+            db.SANPHAMs.Remove(sANPHAM);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
