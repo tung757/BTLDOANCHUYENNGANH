56c7de5483f7241fc37fe0a8cfc784ae8e29c7fb
tung757 <tung24062004@gmail.com>
2025-10-25 16:37:55 +0700

    update_end_to_design

---
+++ WEBLAPTOP/Controllers/DonHangController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEBLAPTOP.Controllers
+{
+    public class DonHangController : Controller
+    {
+        // GET: DonHang
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
