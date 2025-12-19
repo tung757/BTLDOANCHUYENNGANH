16988e795e5d3141b8b98a68df11597d3b69607a
dongtranquoc951 <dongtranquoc951@gmail.com>
2025-12-19 23:13:21 +0700

    comit bug

--- WEBLAPTOP/Models/KHUYENMAI.cs
+++ WEBLAPTOP/Models/KHUYENMAI.cs
@@ -19,13 +19,12 @@
         public int ID_KM { get; set; }
 
         [Required(ErrorMessage = "Giảm giá không được bỏ trống")]
+        
         public int? GiamGia { get; set; }
 
+        [Required(ErrorMessage = "Mô tả không được bỏ trống")]
         [StringLength(4000)]
-        [RegularExpression(@"^[a-zA-Z0-9À-ỹ\s.,!?-]*$",
-            ErrorMessage = "Mô tả chứa ký tự không hợp lệ")]
         public string Mota { get; set; }
-
         public int? TrangThai { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
